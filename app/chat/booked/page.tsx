e0a8a2a1c4389546bc697de1703ea6ab70af2d95
dylanw422 <dylanwestdev@gmail.com>
2025-02-12 10:25:17 -0600

    added scroll

--- app/chat/booked/page.tsx
+++ app/chat/booked/page.tsx
@@ -3,7 +3,7 @@ import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useSidebar } from "@/components/ui/sidebar";
 import { getBookedTrips, getUser } from "@/queries/queries";
-import { Thread } from "@/types/types";
+import type { Thread } from "@/types/types";
 import { useQuery } from "@tanstack/react-query";
 import {
   ArrowLeftRight,
@@ -32,8 +32,11 @@ export default function Booked() {
   }
 
   return (
-    <div>
-      <div className="md:text-xl flex items-center text-center text-lg font-bold border-b p-2">
+    <div className="flex flex-col h-full">
+      <div
+        id="header"
+        className="md:text-xl flex items-center text-center text-lg font-bold border-b p-2"
+      >
         <ChevronsRight
           className={`text-sidebar-foreground hover:cursor-pointer hover:bg-sidebar-accent transition rounded-sm p-1 mr-2 ${
             state === "expanded" ? "md:hidden block" : ""
@@ -44,100 +47,102 @@ export default function Booked() {
           Your Booked Trips
         </h1>
       </div>
-      <div
-        className={`grid gap-4 ${
-          state === "collapsed"
-            ? "grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-            : "md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3"
-        } p-2 md:p-4 overflow-y-scroll`}
-      >
-        {bookedTrips.map((trip: Thread) => (
-          <Card key={trip.id} className="overflow-hidden">
-            <CardHeader className="bg-blue-500 text-white p-4">
-              <CardTitle className="flex items-center justify-between">
-                <span className="flex items-center truncate mr-2">
-                  <MapPin className="mr-2 h-4 w-4" />
-                  <span className="truncate text-sm">{trip.location}</span>
-                </span>
-                <Badge
-                  variant="outline"
-                  className="whitespace-nowrap text-xs bg-white text-black"
-                >
-                  {trip.hotelBooked && trip.flightBooked
-                    ? "Fully Booked"
-                    : "Partially Booked"}
-                </Badge>
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="space-y-2 text-sm p-4">
-              <div className="flex items-center">
-                <Users className="mr-2 h-4 w-4 text-black" />
-                <span>
-                  {trip.review?.guests} Guest
-                  {trip.review?.guests && trip.review.guests > 1 ? "s" : ""}
-                </span>
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center flex-1 min-w-0">
-                  <Hotel className="mr-2 h-4 w-4 text-black flex-shrink-0" />
-                  <span className="truncate">{trip.review?.hotel_name}</span>
-                </div>
-                <Badge
-                  variant="outline"
-                  className={`ml-2 text-xs whitespace-nowrap px-2 py-1 w-[90px] flex items-center justify-center ${
-                    trip.hotelBooked ? "bg-green-400" : "bg-red-300"
-                  }`}
-                >
-                  <span
-                    className={`${
-                      trip.hotelBooked ? "text-green-600" : "text-red-500"
-                    }`}
-                  >
-                    {trip.hotelBooked ? (
-                      <Check className="w-4 h-4" />
-                    ) : (
-                      <X className="w-4 h-4" />
-                    )}
+      <div className="flex-1 overflow-y-auto">
+        <div
+          className={`grid gap-2 p-2 md:p-4 ${
+            state === "collapsed"
+              ? "grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+              : "md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3"
+          }`}
+        >
+          {bookedTrips.map((trip: Thread) => (
+            <Card key={trip.id} className="overflow-hidden rounded-md">
+              <CardHeader className="bg-blue-500 text-white p-4">
+                <CardTitle className="flex items-center justify-between">
+                  <span className="flex items-center truncate mr-2">
+                    <MapPin className="mr-2 h-4 w-4" />
+                    <span className="truncate text-sm">{trip.location}</span>
                   </span>
-                </Badge>
-              </div>
-              <div className="flex items-center justify-between">
+                  <Badge
+                    variant="outline"
+                    className="whitespace-nowrap text-xs bg-white text-black"
+                  >
+                    {trip.hotelBooked && trip.flightBooked
+                      ? "Fully Booked"
+                      : "Partially Booked"}
+                  </Badge>
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="space-y-2 text-sm p-4">
                 <div className="flex items-center">
-                  <Plane className="mr-2 h-4 w-4 text-black" />
-                  <span className="whitespace-nowrap flex items-center gap-1">
-                    {trip.review?.from_airport?.match(/\((.*?)\)/)?.[1]}{" "}
-                    <ArrowLeftRight className="w-3 h-3" />{" "}
-                    {trip.review?.to_airport?.match(/\((.*?)\)/)?.[1]}
+                  <Users className="mr-2 h-4 w-4 text-black" />
+                  <span>
+                    {trip.review?.guests} Guest
+                    {trip.review?.guests && trip.review.guests > 1 ? "s" : ""}
                   </span>
                 </div>
-                <Badge
-                  variant="outline"
-                  className={`ml-2 text-xs whitespace-nowrap px-2 py-1 w-[90px] flex items-center justify-center ${
-                    trip.flightBooked ? "bg-green-400" : "bg-red-300"
-                  }`}
-                >
-                  <span
-                    className={`${
-                      trip.flightBooked ? "text-green-600" : "text-red-500"
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center flex-1 min-w-0">
+                    <Hotel className="mr-2 h-4 w-4 text-black flex-shrink-0" />
+                    <span className="truncate">{trip.review?.hotel_name}</span>
+                  </div>
+                  <Badge
+                    variant="outline"
+                    className={`ml-2 text-xs whitespace-nowrap px-2 py-1 w-[90px] flex items-center justify-center ${
+                      trip.hotelBooked ? "bg-green-400" : "bg-red-300"
                     }`}
                   >
-                    {trip.flightBooked ? (
-                      <Check className="w-4 h-4" />
-                    ) : (
-                      <X className="w-4 h-4" />
-                    )}
+                    <span
+                      className={`${
+                        trip.hotelBooked ? "text-green-600" : "text-red-500"
+                      }`}
+                    >
+                      {trip.hotelBooked ? (
+                        <Check className="w-4 h-4" />
+                      ) : (
+                        <X className="w-4 h-4" />
+                      )}
+                    </span>
+                  </Badge>
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <Plane className="mr-2 h-4 w-4 text-black" />
+                    <span className="whitespace-nowrap flex items-center gap-1">
+                      {trip.review?.from_airport?.match(/$$(.*?)$$/)?.[1]}{" "}
+                      <ArrowLeftRight className="w-3 h-3" />{" "}
+                      {trip.review?.to_airport?.match(/$$(.*?)$$/)?.[1]}
+                    </span>
+                  </div>
+                  <Badge
+                    variant="outline"
+                    className={`ml-2 text-xs whitespace-nowrap px-2 py-1 w-[90px] flex items-center justify-center ${
+                      trip.flightBooked ? "bg-green-400" : "bg-red-300"
+                    }`}
+                  >
+                    <span
+                      className={`${
+                        trip.flightBooked ? "text-green-600" : "text-red-500"
+                      }`}
+                    >
+                      {trip.flightBooked ? (
+                        <Check className="w-4 h-4" />
+                      ) : (
+                        <X className="w-4 h-4" />
+                      )}
+                    </span>
+                  </Badge>
+                </div>
+                <div className="flex items-center">
+                  <Calendar className="mr-2 h-4 w-4 text-black" />
+                  <span>
+                    {trip.review?.dates?.from} - {trip.review?.dates?.to}
                   </span>
-                </Badge>
-              </div>
-              <div className="flex items-center">
-                <Calendar className="mr-2 h-4 w-4 text-black" />
-                <span>
-                  {trip.review?.dates?.from} - {trip.review?.dates?.to}
-                </span>
-              </div>
-            </CardContent>
-          </Card>
-        ))}
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
